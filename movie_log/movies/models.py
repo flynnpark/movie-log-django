ac55d5c4e35c36e5db44bea2e284440f5704396d
Flynn <wphestiraid@gmail.com>
2018-03-23 17:38:18 +0900

    SimpleReview 및 Review 모델 추가
    - TimeStampedModel을 추상클래스로 만듦
    - TimeStampedModel과 User 모델을 외래키로 갖는 SimpleReview 및 Review 모델 추가

--- movie_log/movies/models.py
+++ movie_log/movies/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from movie_log.users import models as user_models
 
 
 class Movie(models.Model):
-
     title = models.CharField(max_length=512)
     subtitle = models.CharField(max_length=512, null=True)
     naver_link = models.URLField(null=True)
@@ -11,3 +11,25 @@ class Movie(models.Model):
     director = models.CharField(max_length=128, null=True)
     actor = models.CharField(max_length=256, null=True)
     user_rating = models.FloatField(null=True)
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SimpleReview(TimeStampedModel):
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    message = models.TextField()
+
+
+class Review(TimeStampedModel):
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    message = models.TextField()
