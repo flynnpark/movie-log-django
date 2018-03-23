866ecdcd074c6063ccc5cd49653c51ff6ce196e8
Flynn <wphestiraid@gmail.com>
2018-03-23 09:58:40 +0900

    Movie 모델 추가

--- movie_log/movies/models.py
+++ movie_log/movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=512)
+    subtitle = models.CharField(max_length=512, null=True)
+    naver_link = models.URLField(null=True)
+    image = models.ImageField()
+    pub_date = models.IntegerField(null=True)
+    director = models.CharField(max_length=128, null=True)
+    actor = models.CharField(max_length=256, null=True)
+    user_rating = models.FloatField(null=True)
